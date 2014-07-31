e5a0f44793ca5933fbc13947745c9f5d8e0cf4e1
Sam Kronick <sam.kronick@gmail.com>
2014-07-31 14:04:47 +0000

    Modified localproxy to work in transparent mod

--- localproxy.py
+++ localproxy.py
@@ -3,6 +3,8 @@
 from libmproxy import controller, proxy
 from libmproxy.proxy.config import ProxyConfig
 from libmproxy.proxy.server import ProxyServer
+from libmproxy import platform
+from libmproxy.proxy.primitives import TransparentUpstreamServerResolver
 
 import os
 import requests
@@ -10,9 +12,15 @@ import json
 import threading
 import socket
 
+TRANSPARENT_SSL_PORTS = [443, 8433]
+
 local_country_codes = ["es"]
 local_region_codes = ["*"]
 
+options = {
+		"static-server-host": "static-01.slow",
+		"static-server-port": "8000",
+		}
 
 class LocalMaster(controller.Master):
 	def __init__(self, server):
@@ -54,6 +62,10 @@ class LocalMaster(controller.Master):
 			def run():
 				# Only worry about HTML for now (and ignore 301/302 redirects)
 				content_type = " ".join(msg.headers["content-type"])
+				print "HOST: {}".format(msg.flow.request.host)
+				print msg.flow.request.headers["host"]
+				msg.flow.request.host = "".join(msg.flow.request.headers["host"])
+
 				if msg.code != 301 and msg.code != 302 and content_type is not None and "text/html" in content_type and msg.flow.request.host not in ["192.168.1.128", "127.0.0.1", "localhost"]:
 					# Try to do a local DNS lookup and search by IP for more accurate results
 					try:
@@ -69,7 +81,11 @@ class LocalMaster(controller.Master):
 						j = json.loads(r.content)
 						country_code = j["country_code"].lower()
 						region_code = j["region_code"].lower()
-						
+					
+						# Append "The" before certain countries to sound more natural
+						if j["country_name"] in ["United States", "United Kingdom", "Netherlands"]:
+							j["country_name"] = "The {}".format(j["country_name"])
+
 						if country_code not in local_country_codes:
 							# Just show a flag
 							extras = ""
@@ -89,7 +105,7 @@ class LocalMaster(controller.Master):
 
 							#msg.content = u"<html><body style='background: #b9b9a9 '><div style='margin-top: 1em; margin-bottom: 1em; text-align: center; font-size: 24pt; font-family: sans-serif; font-weight: bold; color: white; line-height: 1.5em;'>{}<br><img src='http://localhost:8000/flags/{}.png' style='height: 400px'><br>IS NOT LOCAL<br>{}</div></body></html>".format(msg.request.host.upper(), flag, extras)
 							#msg.content = "<html><body style='background: #ff8989; background: -moz-linear-gradient(-45deg, #ff8989 0%, #53cbf1 97%); background: -webkit-gradient(linear, left top, right bottom, color-stop(0%,#ff8989), color-stop(97%,#53cbf1)); background: -webkit-linear-gradient(-45deg, #ff8989 0%,#53cbf1 97%); background: -o-linear-gradient(-45deg, #ff8989 0%,#53cbf1 97%); background: -ms-linear-gradient(-45deg, #ff8989 0%,#53cbf1 97%); background: linear-gradient(135deg, #ff8989 0%,#53cbf1 97%); filter: progid:DXImageTransform.Microsoft.gradient( startColorstr=\'#ff8989\', endColorstr=\'#53cbf1\',GradientType=1 ); '><div style='margin-top: 1em; margin-bottom: 1em; text-align: center; font-size: 24pt; font-family: sans-serif; font-weight: bold; color: white; line-height: 1.5em;'>{}<br><img src='http://localhost:8000/flags/{}.png' style='height: 400px'><br>IS NOT LOCAL<br>{}</div></body></html>".format(msg.request.host.upper(), country_code, extras)
-							msg.content = u"<html><body style='background: url(http://127.0.0.1:8000/flags/{}.png); background-size: 100%;'><div style='width: 900px; height: 200px; margin: auto; position: absolute; left:0; right:0; top:0; bottom:0; text-align: center; font-size: 36pt; font-family: sans-serif; font-weight: bold; color: white; line-height: 1.5em; text-shadow: black 0 0 40px;'><div style='background: rgba(0,0,0,.5); width: auto;'>{}<br>IS NOT LOCAL<br>{}</div></div></body></html>".format(flag, msg.flow.request.host.upper(), extras)
+							msg.content = u"<html><body style='background: url(http://{}:{}/flags/{}.png); background-size: 100%;'><div style='width: 900px; height: 200px; margin: auto; position: absolute; left:0; right:0; top:0; bottom:0; text-align: center; font-size: 36pt; font-family: sans-serif; font-weight: bold; color: white; line-height: 1.5em; text-shadow: black 0 0 40px;'><div style='background: rgba(0,0,0,.5); width: auto;'>{}<br>IS NOT LOCAL<br>{}</div></div></body></html>".format(options["static-server-host"], options["static-server-port"], flag, msg.flow.request.host.upper(), extras)
 						
 							# Force unicode
 							msg.content = msg.content.encode("utf-8")
@@ -104,7 +120,7 @@ class LocalMaster(controller.Master):
 							msg.headers["Pragma"] = ["no-cache"]
 							msg.headers["Cache-Control"] = ["no-cache, no-store"]
 					except ValueError as e:
-						msg.content = "<html><body style='background: url(http://127.0.0.1:8000/flags/missing.png); background-size: 100%;'><div style='width: 900px; height: 200px; margin: auto; position: absolute; left:0; right:0; top:0; bottom:0; text-align: center; font-size: 36pt; font-family: sans-serif; font-weight: bold; color: white; line-height: 1.5em; text-shadow: black 0 0 40px;'><div style='background: rgba(0,0,0,.5); width: auto;'>I DON'T KNOW WHERE I AM<br><span style='font-size: 50%; line-height: 1.75em;'>Check back later to find out if<br>{}<br>is local.</span></div></div></body></html>".format(msg.request.host.upper())
+						msg.content = "<html><body style='background: url(http://{}:{}/flags/missing.png); background-size: 100%;'><div style='width: 900px; height: 200px; margin: auto; position: absolute; left:0; right:0; top:0; bottom:0; text-align: center; font-size: 36pt; font-family: sans-serif; font-weight: bold; color: white; line-height: 1.5em; text-shadow: black 0 0 40px;'><div style='background: rgba(0,0,0,.5); width: auto;'>I DON'T KNOW WHERE I AM<br><span style='font-size: 50%; line-height: 1.75em;'>Check back later to find out if<br>{}<br>is local.</span></div></div></body></html>".format(options["static-server-host"], options["static-server-port"], msg.flow.request.host.upper())
 						msg.headers["content-encoding"] = [""]
 						# Don't cache
 						msg.headers["Pragma"] = ["no-cache"]
@@ -126,7 +142,10 @@ class LocalMaster(controller.Master):
 
 config = ProxyConfig(
 	#certs = [os.path.expanduser("~/.mitmproxy/mitmproxy-ca.pem")]
-	confdir = "~/.mitmproxy"
+	confdir = "~/.mitmproxy",
+    http_form_in = "relative",
+	http_form_out = "relative",
+    get_upstream_server = TransparentUpstreamServerResolver(platform.resolver(), TRANSPARENT_SSL_PORTS)
 )
 #config = None
 server = ProxyServer(config, 8080)
