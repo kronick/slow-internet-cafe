d21a6c9ed25458132df1473548c0404ef05c22cc
Sam Kronick <sam.kronick@gmail.com>
2014-07-31 16:21:50 +0000

    Reorganized folder structure and started implementing jinja2 templates

--- proxies/localproxy.py
+++ proxies/localproxy.py
@@ -12,6 +12,9 @@ import json
 import threading
 import socket
 
+from jinja2 import Environment, FileSystemLoader
+template_env = Environment(loader=FileSystemLoader("templates"))
+
 TRANSPARENT_SSL_PORTS = [443, 8433]
 
 local_country_codes = ["es"]
@@ -82,30 +85,31 @@ class LocalMaster(controller.Master):
 						country_code = j["country_code"].lower()
 						region_code = j["region_code"].lower()
 					
-						# Append "The" before certain countries to sound more natural
-						if j["country_name"] in ["United States", "United Kingdom", "Netherlands"]:
-							j["country_name"] = "The {}".format(j["country_name"])
+						# Append "the" before certain countries to sound more natural
+						if j["country_name"] in ["United States", "United Kingdom", "Netherlands"] and j["region_name"] == "" and j["city"] == "":
+							j["country_name"] = "the {}".format(j["country_name"])
 
 						if country_code not in local_country_codes:
 							# Just show a flag
-							extras = ""
+							notes = ""
 							if j["region_name"] != "":
 								if j["city"] != "":
-									extras = u"It is hosted in {}, {}, {}.".format(j["city"], j["region_name"], j["country_name"])
+									notes = u"It is hosted in {}, {}, {}.".format(j["city"], j["region_name"], j["country_name"])
 								else:
-									extras = u"It is hosted in {}, {}.".format(j["region_name"], j["country_name"])
+									notes = u"It is hosted in {}, {}.".format(j["region_name"], j["country_name"])
 							else:
-								extras = u"It is hosted in {}.".format(j["country_name"])
+								notes = u"It is hosted in {}.".format(j["country_name"])
 
-							extras = u"<span style='font-size: 50%'>" + extras + u"</span>"
+							#notes = u"<span style='font-size: 50%'>" + notes + u"</span>"
 
 							flag = country_code
 							if country_code == "us" and region_code != "":
 								flag = u"{}-{}".format(country_code, region_code)
 
-							#msg.content = u"<html><body style='background: #b9b9a9 '><div style='margin-top: 1em; margin-bottom: 1em; text-align: center; font-size: 24pt; font-family: sans-serif; font-weight: bold; color: white; line-height: 1.5em;'>{}<br><img src='http://localhost:8000/flags/{}.png' style='height: 400px'><br>IS NOT LOCAL<br>{}</div></body></html>".format(msg.request.host.upper(), flag, extras)
-							#msg.content = "<html><body style='background: #ff8989; background: -moz-linear-gradient(-45deg, #ff8989 0%, #53cbf1 97%); background: -webkit-gradient(linear, left top, right bottom, color-stop(0%,#ff8989), color-stop(97%,#53cbf1)); background: -webkit-linear-gradient(-45deg, #ff8989 0%,#53cbf1 97%); background: -o-linear-gradient(-45deg, #ff8989 0%,#53cbf1 97%); background: -ms-linear-gradient(-45deg, #ff8989 0%,#53cbf1 97%); background: linear-gradient(135deg, #ff8989 0%,#53cbf1 97%); filter: progid:DXImageTransform.Microsoft.gradient( startColorstr=\'#ff8989\', endColorstr=\'#53cbf1\',GradientType=1 ); '><div style='margin-top: 1em; margin-bottom: 1em; text-align: center; font-size: 24pt; font-family: sans-serif; font-weight: bold; color: white; line-height: 1.5em;'>{}<br><img src='http://localhost:8000/flags/{}.png' style='height: 400px'><br>IS NOT LOCAL<br>{}</div></body></html>".format(msg.request.host.upper(), country_code, extras)
-							msg.content = u"<html><body style='background: url(http://{}:{}/flags/{}.png); background-size: 100%;'><div style='width: 900px; height: 200px; margin: auto; position: absolute; left:0; right:0; top:0; bottom:0; text-align: center; font-size: 36pt; font-family: sans-serif; font-weight: bold; color: white; line-height: 1.5em; text-shadow: black 0 0 40px;'><div style='background: rgba(0,0,0,.5); width: auto;'>{}<br>IS NOT LOCAL<br>{}</div></div></body></html>".format(options["static-server-host"], options["static-server-port"], flag, msg.flow.request.host.upper(), extras)
+							template = template_env.get_template('local/notlocal.html')
+							msg.content = template.render(flag=flag, host=msg.flow.request.host, notes=notes)
+
+							#msg.content = u"<html><body style='background: url(http://{}:{}/flags/{}.png); background-size: 100%;'><div style='width: 900px; height: 200px; margin: auto; position: absolute; left:0; right:0; top:0; bottom:0; text-align: center; font-size: 36pt; font-family: sans-serif; font-weight: bold; color: white; line-height: 1.5em; text-shadow: black 0 0 40px;'><div style='background: rgba(0,0,0,.5); width: auto;'>{}<br>IS NOT LOCAL<br>{}</div></div></body></html>".format(options["static-server-host"], options["static-server-port"], flag, msg.flow.request.host.upper(), extras)
 						
 							# Force unicode
 							msg.content = msg.content.encode("utf-8")
@@ -120,7 +124,10 @@ class LocalMaster(controller.Master):
 							msg.headers["Pragma"] = ["no-cache"]
 							msg.headers["Cache-Control"] = ["no-cache, no-store"]
 					except ValueError as e:
-						msg.content = "<html><body style='background: url(http://{}:{}/flags/missing.png); background-size: 100%;'><div style='width: 900px; height: 200px; margin: auto; position: absolute; left:0; right:0; top:0; bottom:0; text-align: center; font-size: 36pt; font-family: sans-serif; font-weight: bold; color: white; line-height: 1.5em; text-shadow: black 0 0 40px;'><div style='background: rgba(0,0,0,.5); width: auto;'>I DON'T KNOW WHERE I AM<br><span style='font-size: 50%; line-height: 1.75em;'>Check back later to find out if<br>{}<br>is local.</span></div></div></body></html>".format(options["static-server-host"], options["static-server-port"], msg.flow.request.host.upper())
+						template = template_env.get_template('local/error.html')
+						msg.content = template.render(host=msg.flow.request.host)
+
+						#msg.content = "<html><body style='background: url(http://{}:{}/flags/missing.png); background-size: 100%;'><div style='width: 900px; height: 200px; margin: auto; position: absolute; left:0; right:0; top:0; bottom:0; text-align: center; font-size: 36pt; font-family: sans-serif; font-weight: bold; color: white; line-height: 1.5em; text-shadow: black 0 0 40px;'><div style='background: rgba(0,0,0,.5); width: auto;'>I DON'T KNOW WHERE I AM<br><span style='font-size: 50%; line-height: 1.75em;'>Check back later to find out if<br>{}<br>is local.</span></div></div></body></html>".format(options["static-server-host"], options["static-server-port"], msg.flow.request.host.upper())
 						msg.headers["content-encoding"] = [""]
 						# Don't cache
 						msg.headers["Pragma"] = ["no-cache"]
