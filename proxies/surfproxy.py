0eb58a758d96512cd671db767d5c7825cb73b12d
Sam Kronick <sam.kronick@gmail.com>
2014-08-14 19:53:34 +0200

    Fixing URL resolution on surfproxy

--- proxies/surfproxy.py
+++ proxies/surfproxy.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 from random import random, randrange, randint, shuffle
-import os
+import os, threading
 
 from libmproxy import controller, proxy
 from libmproxy.proxy.config import ProxyConfig
@@ -14,8 +14,8 @@ template_env = Environment(loader=FileSystemLoader("templates"))
 
 # TODO: Set these based on the surf report from San Diego or somewhere
 weather_params = {
-    "REVERBERATE": 0.8,
-    "WAIT": 0.1,
+    "REVERBERATE": 0.5,
+    "WAIT": 0.3,
     "SLOP": 0.1,
     "CHOP": 1.0
 }
@@ -47,19 +47,23 @@ class SurfMaster(controller.Master):
 
     def handle_response(self, msg):
         # Process replies from Internet servers to clients
-        # ------------------------------------------------
+        
         try:
             # Only worry about HTML for now
             content_type = " ".join(msg.headers["content-type"])
             if content_type is not None and "text/html" in content_type:
+                req = msg.flow.request
+                client_ip = msg.flow.client_conn.address.host
+                url = "{}://{}{}".format(req.get_scheme(), "".join(req.headers["host"]), req.path)
+
                 #print msg.headers["content-encoding"]
                 #print dir(msg)
                 #print msg.get_decoded_content()
 
-                new = not waveQueue.has_key(msg.flow.request.get_url())
-                waveQueue[msg.flow.request.get_url()] = msg.get_decoded_content()
+                new = not waveQueue.has_key(url)
+                waveQueue[url] = msg.get_decoded_content()
 
-                if new or random() < weather_params["WAIT"]:
+                if random() < weather_params["WAIT"]:
                     # Tranquilo...
                     msg.content = "<html><head><META HTTP-EQUIV='CACHE-CONTROL' CONTENT='NO-CACHE'></head><body style='margin: 0px;'><a href='javascript:window.location.reload()'><img src='http://media0.giphy.com/media/u538oVJPQ0Rzi/200.gif' style='border: 0; width: 100%; height: 100%'></a></body></html>"
                     msg.encode(msg.headers["content-encoding"][0])
@@ -67,27 +71,27 @@ class SurfMaster(controller.Master):
                 else:
                     output = ""
                     survivors = {}
-                    for url in waveQueue:
+                    for u in waveQueue:
                         # Always output the current page, maybe some extra junk
-                        if msg.flow.request.get_url() == url or random() < weather_params["SLOP"]:
-                            thispage = waveQueue[url]
+                        if url == u or random() < weather_params["SLOP"]:
+                            thispage = waveQueue[u]
                             insertion_point = randint(0, len(output))
 
                             cut_start = randint(0, len(thispage))
                             cut_stop  = randint(cut_start, len(thispage))
-                            if msg.flow.request.get_url() == url:
+                            if url == u:
                                 cut_start = 0
                                 cut_stop = len(thispage)
                             
 
-                            print "Inserting [{}:{}] from {} at {}".format(cut_start, cut_stop, url, insertion_point)
+                            print "Inserting [{}:{}] from {} at {}".format(cut_start, cut_stop, u, insertion_point)
                             output = output[:insertion_point] + thispage[cut_start:cut_stop] + output[insertion_point:]
 
                             # Give this page a chance to surface again
                             if new or random() < weather_params["REVERBERATE"]:
-                                survivors[url] = waveQueue[url] 
+                                survivors[u] = waveQueue[u] 
                         else:
-                            survivors[url] = waveQueue[url]
+                            survivors[u] = waveQueue[u]
 
                     msg.content = output
                     msg.encode(msg.headers["content-encoding"][0])
@@ -98,8 +102,8 @@ class SurfMaster(controller.Master):
 
                     # Update the list
                     waveQueue.clear()
-                    for url in survivors:
-                        waveQueue[url] = survivors[url]
+                    for u in survivors:
+                        waveQueue[u] = survivors[u]
                     
 
                 # elif len(waveQueue) > 1 and random() < 0.5:
@@ -116,6 +120,7 @@ class SurfMaster(controller.Master):
             print e
 
         msg.reply()
+        
 
 
 
